cc0f702113d3a149e336a5cb02633f1e71357356
Phucphan <phuc20513@gmail.com>
2024-10-25 20:58:40 +0700

    upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const moveButton = document.getElementById("moveButton");
+const thankButton = document.getElementById("thankButton");
+
+moveButton.addEventListener("mouseover", function() {
+    const buttonWidth = moveButton.offsetWidth; 
+    const buttonHeight = moveButton.offsetHeight; 
+    
+    const randomX = Math.floor(Math.random() * 700); 
+    const randomY = Math.floor(Math.random() * 390); 
+
+    moveButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
+});
+
+thankButton.addEventListener("click", function() {
+    alert("Xin cảm ơn, xin cảm ơn, xin cảm ơn!!");
+});

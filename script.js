12eb11156415d0fd7d6f5e8b473a52734ff7bb57
JasonP205 <phuc20513@gmail.com>
2024-10-25 21:14:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ moveButton.addEventListener("mouseover", function() {
     moveButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
 });
 
+moveButton.addEventListener("click", function() {
+    const buttonWidth = moveButton.offsetWidth; 
+    const buttonHeight = moveButton.offsetHeight; 
+    
+    const randomX = Math.floor(Math.random() * 700); 
+    const randomY = Math.floor(Math.random() * 390); 
+
+    moveButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
+});
+
 thankButton.addEventListener("click", function() {
     alert("Xin cảm ơn, xin cảm ơn, xin cảm ơn!!");
 });
